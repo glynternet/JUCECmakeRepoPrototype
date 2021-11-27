1cc58788635d77117c45cf62f64e4945f20e7228
Glyn Owen Hanmer <glynhanmer@gmail.com>
2021-11-27 11:42:07 +0000

    Something that seems to be working

--- Apps/AudioAppTemplate/Source/MainComponent.h
+++ Apps/AudioAppTemplate/Source/MainComponent.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #include "CommonHeader.h"
+#include "../Libs/BTrack/BTrack.h"
+#include <cmath>
 
 namespace AudioApp
 {
 class MainComponent : public juce::AudioAppComponent
 {
+
+    static const int btrackFrameSize = 512;
+    static const int btrackHopSize = 256;
+
 public:
     MainComponent();
     ~MainComponent();
@@ -21,6 +27,12 @@ private:
     juce::AudioDeviceSelectorComponent selector {
         deviceManager, 2, 2, 2, 2, false, false, true, false};
     WhiteNoise::Oscillator noise;
+
+    BTrack b { btrackHopSize, btrackFrameSize };
+    int count = 0;
+    // hop size and frame size. For easiest maintenance we want
+    // frame size to be a full buffer size from the audio settings
+//    b = BTrack(btrackHopSize, btrackFrameSize);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
