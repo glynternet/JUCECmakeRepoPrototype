2f846278e32236762b3e30da81a63f41d02a0355
Glyn Owen Hanmer <glynhanmer@gmail.com>
2021-12-01 22:24:19 +0000

    Centralise logging logic

--- Apps/AudioAppTemplate/Source/MainComponent.h
+++ Apps/AudioAppTemplate/Source/MainComponent.h
@@ -26,6 +26,7 @@ private:
     WhiteNoise::Oscillator noise;
 
     juce::Label message;
+    void log(const String &message);
 
     // File play
     enum TransportState
@@ -38,22 +39,21 @@ private:
 
     TransportState state;
 
-    std::unique_ptr<juce::FileChooser> fileChooser_;
-
     void openButtonClicked();
+    std::unique_ptr<juce::FileChooser> fileChooser_;
     void chooserClosed(const juce::FileChooser& chooser);
     void playButtonClicked();
     void stopButtonClicked();
     void transportStateChanged(TransportState newState);
+
     void changeListenerCallback (juce::ChangeBroadcaster *source) override;
 
     void connectOSCSender();
-
     juce::TextButton openButton;
     juce::TextButton playButton;
     juce::TextButton stopButton;
-    juce::AudioFormatManager formatManager;
 
+    juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> playSource;
     juce::AudioTransportSource transport;
 
@@ -62,19 +62,20 @@ private:
     int btrackFrameSize = 512;
     int btrackHopSize = 256;
     BTrack b { btrackHopSize, btrackFrameSize };
-    uint64_t beats = 0;
 
+    uint64_t beats = 0;
     uint8_t beat = 0;
     juce::Label tempoLabel;
     juce::int64 lastTime = juce::Time::currentTimeMillis();
     double diffEwma = 0;
+
     void repeatFunc(int interval, int count, std::function<void()> call);
 
     double ewma(double current, double nextValue, double alpha) const;
-
     juce::TextButton connectOSCButton;
     // Probably worth taking a look at the AVVAOSCSender class from the legacy repo
     juce::OSCSender sender;
+
     bool senderConnected = false;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
