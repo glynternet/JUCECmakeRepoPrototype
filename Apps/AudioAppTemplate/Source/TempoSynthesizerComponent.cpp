02d9e9cf96c92ea6a01746fe38d4ef898e415ef8
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2022-03-27 20:55:44 +0100

    Set multiplier through UI (#6)

--- Apps/AudioAppTemplate/Source/TempoSynthesizerComponent.cpp
+++ Apps/AudioAppTemplate/Source/TempoSynthesizerComponent.cpp
@@ -1,30 +1,78 @@
 #include "TempoSynthesizerComponent.h"
-#include "LogOutputComponent.h"
+#include "Math.h"
 
 namespace AudioApp
 {
-    const int multiplier = 4;
     static const int fadeIncrements = 8;
+    int halfHeight;
+    int segmentWidth;
 
     TempoSynthesizerComponent::TempoSynthesizerComponent() {
-        label.setColour (juce::Label::textColourId, colour);
-        label.setJustificationType(juce::Justification::centred);
-        addAndMakeVisible(label);
+        up.onClick = [this](){
+            // 256 is max because current beat is represented as uint8
+            if (nextMultiplierIndex < 8) {
+                setNextMultiplierIndex(nextMultiplierIndex + 1);
+            }
+        };
+        juce::Path upShape;
+        upShape.addTriangle(0.0, 0.0, 1.0, 1.0, 0.0, 2.0);
+        up.setShape(upShape, true, false, false);
+        up.setOutline(juce::Colours::transparentWhite, 3);
+        addAndMakeVisible(up);
+        down.onClick = [this](){
+            if (nextMultiplierIndex > 0) {
+                setNextMultiplierIndex(nextMultiplierIndex - 1);
+            }
+        };
+        juce::Path downShape;
+        downShape.addTriangle(1.0, 0.0, 0.0, 1.0, 1.0, 2.0);
+        down.setShape(downShape, true, false, false);
+        down.setOutline(juce::Colours::transparentWhite, 3);
+        addAndMakeVisible(down);
+        for (int i = 0; i < 9; ++i) {
+            multiplierValues[i] = ipow(2, i);
+            juce::ShapeButton &button = multiplierValueButtons[i];
+            button.onClick = [this, i]() {
+                setNextMultiplierIndex(i);
+            };
+            juce::Path rect;
+            rect.addRectangle(0.0, 0.0, 1.0, 1.0);
+            button.setShape(rect, true, false, false);
+            button.setOutline(juce::Colours::transparentWhite, 3);
+            addAndMakeVisible(button);
+        }
+        setMultiplierIndex(2);
+        setNextMultiplierIndex(2);
         juce::HighResolutionTimer::startTimer(1);
         juce::Timer::startTimerHz(60);
+    }
+
+    void TempoSynthesizerComponent::setMultiplierIndex(int m) {
+        multiplierValueButtons[multiplierIndex].setColours(juce::Colours::grey, juce::Colours::grey, juce::Colours::grey);
+        multiplierIndex = m;
+        multiplierValueButtons[multiplierIndex].setColours(juce::Colours::white, juce::Colours::white, juce::Colours::white);
+    }
+
+    void TempoSynthesizerComponent::setNextMultiplierIndex(int m) {
+        multiplierValueButtons[nextMultiplierIndex].setOutline(juce::Colours::transparentWhite, 3);
+        nextMultiplierIndex = m;
+        multiplierValueButtons[nextMultiplierIndex].setOutline(juce::Colours::white, 3);
     }
 
     void TempoSynthesizerComponent::paint(Graphics& g) {
         g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
         g.setColour(colour);
-        auto segmentWidth = getLocalBounds().getWidth()/multiplier;
-        g.fillRect(currentBeat * segmentWidth,0,segmentWidth,getLocalBounds().getHeight());
-        label.setColour (juce::Label::textColourId, colour);
-        label.setText(content, juce::dontSendNotification);
+        g.fillRect((int)((float)getWidth() * (float)currentBeat / (float)multiplierValues[multiplierIndex]), 0, segmentWidth, halfHeight);
     }
 
     void TempoSynthesizerComponent::resized() {
-        label.setBounds(getLocalBounds());
+        halfHeight = getHeight() / 2;
+        auto rect = getLocalBounds().withTrimmedTop(halfHeight);
+        up.setBounds(rect.removeFromRight(30));
+        down.setBounds(rect.removeFromLeft(30));
+        for (int i = 0; i < 9; ++i) {
+            multiplierValueButtons[i].setBounds(rect.removeFromLeft(rect.getWidth()/(9-i)));
+        }
     }
 
     void TempoSynthesizerComponent::timerCallback() {
@@ -59,34 +107,37 @@ namespace AudioApp
         // extra beats rather than downsampling to less.
         updateBeat(0);
 
-        for (uint8_t i = 1; i < multiplier; ++i) {
+        if (multiplierIndex != nextMultiplierIndex) {
+            setMultiplierIndex(nextMultiplierIndex);
+        }
+
+        int multiplier = multiplierValues[multiplierIndex];
+        for (uint8_t i = 0; i < multiplier - 1; ++i) {
+            uint8_t beat = i + 1;
             scheduledBeats.push_back(scheduledBeat{
-                timeOfBeat + (diffEwma / (double) multiplier * (double)i),
-                i,
+                timeOfBeat + (diffEwma / (double) multiplier * (double)beat),
+                beat,
             });
         }
 
         flash(0.75f * (float)diffEwma);
+        segmentWidth = getWidth() / multiplier;
+        dirty = true;
     }
 
     void TempoSynthesizerComponent::flash(float duration) {
         Repeat::repeatFunc(duration/fadeIncrements, fadeIncrements, [this](int i){
-            this->updateLabelColour(juce::Colours::white.interpolatedWith(juce::Colours::grey, (float) i / float(fadeIncrements-1)));
+            this->updateColour(juce::Colours::white.interpolatedWith(juce::Colours::grey, (float) i / float(fadeIncrements-1)));
         });
     }
 
     void TempoSynthesizerComponent::updateBeat(uint8_t beat) {
         currentBeat = beat;
-        content = "● " + std::to_string(beat);
         dirty = true;
     }
 
-    void TempoSynthesizerComponent::updateLabelColour(juce::Colour newColour) {
+    void TempoSynthesizerComponent::updateColour(juce::Colour newColour) {
         colour = newColour;
         dirty = true;
-    }
-
-    double TempoSynthesizerComponent::ewma(double current, double nextValue, double alpha) {
-        return alpha * nextValue + (1 - alpha) * current;
     }
 }
