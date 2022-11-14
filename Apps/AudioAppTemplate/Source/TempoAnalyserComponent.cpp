a634fc8fa5362191e239bcf9deef4fe0d254b969
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2022-11-14 20:49:23 +0000

    ♻ extract out FlashBox component (#21)

--- Apps/AudioAppTemplate/Source/TempoAnalyserComponent.cpp
+++ Apps/AudioAppTemplate/Source/TempoAnalyserComponent.cpp
@@ -2,23 +2,8 @@
 
 namespace AudioApp
 {
-    static const int fadeIncrements = 16;
-
     TempoAnalyserComponent::TempoAnalyserComponent() {
-        startTimerHz(60);
         lastTime = juce::Time::getMillisecondCounterHiRes();
-    }
-
-    void TempoAnalyserComponent::paint(Graphics& g) {
-        g.fillAll (colour);
-    }
-
-    void TempoAnalyserComponent::resized() {}
-
-    void TempoAnalyserComponent::timerCallback() {
-        if (dirty.exchange(false)) {
-            repaint();
-        }
     }
 
     void TempoAnalyserComponent::processAudioFrame (double* frame) {
@@ -30,22 +15,10 @@ namespace AudioApp
             if (onBeat != nullptr) {
                 onBeat(diff);
             }
-            flash(0.75f * (float)diff);
         }
     }
 
     void TempoAnalyserComponent::updateSamplePerBlockExpected(int samplePerBlockExpected){
         btrack.updateHopAndFrameSize(samplePerBlockExpected / 2, samplePerBlockExpected);
-    }
-
-    void TempoAnalyserComponent::flash(float duration) {
-        Repeat::repeatFunc(duration/fadeIncrements, fadeIncrements, [this](int i){
-            this->setColour(juce::Colours::white.interpolatedWith(juce::Colours::grey, (float) i / float(fadeIncrements-1)));
-        });
-    }
-
-    void TempoAnalyserComponent::setColour(juce::Colour newColour) {
-        colour = newColour;
-        dirty = true;
     }
 }
