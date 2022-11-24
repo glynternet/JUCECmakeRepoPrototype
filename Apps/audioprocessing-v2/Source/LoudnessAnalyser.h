6bd91bdbee4276ccf04506e7b251008ee6be5c62
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2022-11-24 21:37:16 +0000

    Extract out LoudnessAnalyser

---
+++ Apps/audioprocessing-v2/Source/LoudnessAnalyser.h
@@ -0,0 +1,51 @@
+//
+// Created by glynh on 24/11/2022.
+//
+
+#ifndef JUCECMAKEREPO_LOUDNESSANALYSER_H
+#define JUCECMAKEREPO_LOUDNESSANALYSER_H
+
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_dsp/juce_dsp.h>
+#include "ValueShaper.h"
+#include "MovingAverage.h"
+#include "TailOff.h"
+
+class LoudnessAnalyser : juce::Timer {
+public:
+    explicit LoudnessAnalyser(std::function<void(float)> onLoudnessResult, float processRate, float processingBandIndexLow,
+                              float processingBandIndexHigh, float movingAverageInitialWindow,
+                              float initialDecayExponent);
+    void timerCallback() override;
+    void setProcessRateHz(int rate);
+
+    void pushNextSampleIntoFifo(float sample) noexcept;
+
+    std::function<void(float)> onLoudnessResult;
+    // TODO(glynternet): this isn't an index, sort out abstraction and naming
+    double processingBandIndexLow;
+    double processingBandIndexHigh;
+    ValueShaper valueShaper{0.0f, 1.0f, 0.0f, 1.0f};
+    MovingAverage movingAverage;
+    TailOff decayLength;
+
+private:
+    void fftTimerCallback();
+    float calculateLevel();
+    float calculateLoudness(float *data, int dataSize);
+
+    bool nextFFTBlockReady = false;
+    dsp::WindowingFunction<float> window{fftSize, dsp::WindowingFunction<float>::hann};
+    dsp::FFT forwardFFT{fftOrder};
+
+    enum {
+        fftOrder = 8, fftSize = 1 << fftOrder
+    };
+    float fftData[2 * fftSize];
+    float fifo[fftSize];
+    int fifoIndex = 0;
+
+};
+
+
+#endif //JUCECMAKEREPO_LOUDNESSANALYSER_H
