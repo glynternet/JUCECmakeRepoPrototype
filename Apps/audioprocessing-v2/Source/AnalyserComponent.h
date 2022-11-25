619a7a3a817646cfa4ba016d048313196fabc743
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2022-11-25 21:44:10 +0000

    Extract out LoudnessAnalyserSettings

--- Apps/audioprocessing-v2/Source/AnalyserComponent.h
+++ Apps/audioprocessing-v2/Source/AnalyserComponent.h
@@ -50,6 +50,7 @@
 #include "JuceHeader.h"
 #include "LabelledSlider.h"
 #include "LoudnessAnalyser.h"
+#include "LoudnessAnalyserSettings.h"
 #include "RemoteAddressComponent.h"
 #include "ValueHistoryComponent.h"
 #include "../../mbk/Source/AudioSourceComponent.h"
@@ -73,36 +74,19 @@ public:
             bool visible = drawValueHistoryToggle.getToggleState();
             valueHistoryComp.setVisible(visible);
             audioSource.setVisible(visible);
-            processingBandSlider.setVisible(visible);
-            shaperInSlider.setVisible(visible);
-            movingAverageSlider.setVisible(visible);
-            decayLengthSlider.setVisible(visible);
             addressComponent.setVisible(visible);
             cpuUsageLabel.setVisible(visible);
             cpuUsageText.setVisible(visible);
-            processRateSlider.setVisible(visible);
         };
+
+        addAndMakeVisible(loudnessAnalyserSettings);
 
         setSize(900, 500);
 
         setAudioChannels(2, 2);
 
-        setupProcessingBandSlider(processingBandSlider);
-        setupShaperInSlider(shaperInSlider);
-        setupMovingAverageSlider(movingAverageSlider);
-        setupDecayLengthSlider(decayLengthSlider);
-
         addAndMakeVisible(&addressComponent);
-        addressComponent.onTextChange = [this](String text) { setRemoteAddressFromString(text); };
-
-        addAndMakeVisible(&processRateSlider);
-        Slider &slider = processRateSlider.getSlider();
-        slider.setRange(5, 70);
-        slider.setValue(initialProcessRateHz, dontSendNotification);
-        // TODO(glynternet): do we need to stop the old timer or does doing this just change the frequency?
-        slider.onValueChange = [this, &slider]() {
-            loudnessAnalyser.setProcessRateHz((int) slider.getValue());
-        };
+        addressComponent.onTextChange = [this](const String &text) { setRemoteAddressFromString(text); };
 
         startTimerHz(30.f);
     }
@@ -123,23 +107,9 @@ public:
 
         resizeAudioSettings(bounds.removeFromLeft(500));
         valueHistoryComp.setBounds(bounds);
-
-        resizeSliderGroup(bounds);
+        drawValueHistoryToggle.setBounds(bounds);
+        loudnessAnalyserSettings.setBounds(bounds.getProportion(juce::Rectangle(0.f, 0.f, 0.6f, 1.f)));
         addressComponent.setBounds(getLocalBounds().removeFromBottom(20).removeFromRight(200));
-    }
-
-    void resizeSliderGroup(Rectangle<int> container) {
-        constexpr int leftMargin = 10;
-        constexpr int topMargin = 10;
-        constexpr int height = 40;
-        const int width = container.proportionOfWidth(0.65f);
-        const auto leftEdge = container.getPosition().getX() + leftMargin;
-        drawValueHistoryToggle.setBounds(leftEdge, topMargin, width, height);
-        processingBandSlider.setBounds(leftEdge, topMargin + 1 * height, width, height);
-        shaperInSlider.setBounds(leftEdge, topMargin + 2 * height, width, height);
-        movingAverageSlider.setBounds(leftEdge, topMargin + 3 * height, width, height);
-        decayLengthSlider.setBounds(leftEdge, topMargin + 4 * height, width, height);
-        processRateSlider.setBounds(leftEdge, topMargin + 5 * height, width, height);
     }
 
     void resizeAudioSettings(Rectangle<int> container) {
@@ -148,9 +118,6 @@ public:
         cpuUsageText.setBounds(cpuSpace);
         audioSource.setBounds(container);
     }
-
-    //==============================================================================
-    // AudioAppComponent functions
 
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override {
         audioSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
@@ -176,74 +143,12 @@ public:
         g.fillAll(Colours::black);
     }
 
-private:
-    void setupProcessingBandSlider(LabelledSlider &slider) {
-        addAndMakeVisible(slider);
-        Slider &innerSlider = slider.getSlider();
-        innerSlider.setSliderStyle(Slider::TwoValueHorizontal);
-        innerSlider.setRange(0.0f, 1.0f);
-        innerSlider.setSkewFactor(0.5f);  // TODO: change this from a relatively arbitrary value
-        innerSlider.setMinValue(initialProcessingBandLow);
-        innerSlider.setMaxValue(initialProcessingBandHigh);
-        slider.onValueChange = [this, &innerSlider] {
-            this->loudnessAnalyser.processingBandIndexLow = innerSlider.getMinValue();
-            this->loudnessAnalyser.processingBandIndexHigh = innerSlider.getMaxValue();
-        };
-    }
-
-    void setupShaperInSlider(LabelledSlider &slider) {
-        addAndMakeVisible(&slider);
-        auto &innerSlider = slider.getSlider();
-        innerSlider.setSliderStyle(Slider::TwoValueHorizontal);
-        innerSlider.setRange(-0.1f, 1.1f);
-        innerSlider.setMinValue(0.1f);
-        innerSlider.setMaxValue(0.8f);
-        innerSlider.setSkewFactor(0.3f);  // TODO: change this from a relatively arbitrary value
-
-        slider.onValueChange = [this, &slider]() {
-            loudnessAnalyser.valueShaper.setInMin((float) slider.getSlider().getMinValue());
-            loudnessAnalyser.valueShaper.setInMax((float) slider.getSlider().getMaxValue());
-        };
-    }
-
-    void setupMovingAverageSlider(LabelledSlider &slider) {
-        addAndMakeVisible(&slider);
-        auto &innerSlider = slider.getSlider();
-        innerSlider.setValue(movingAverageInitialWindow);
-        innerSlider.setRange(1, 7, 1);
-        slider.onValueChange = [this, &innerSlider]() {
-            loudnessAnalyser.movingAverage.setPeriod((int) innerSlider.getValue());
-        };
-    }
-
-    void setupDecayLengthSlider(LabelledSlider &slider) {
-        addAndMakeVisible(&slider);
-        auto &innerSlider = slider.getSlider();
-        innerSlider.setValue(initialDecayExponent);
-        innerSlider.setRange(TailOff::minExponent, TailOff::maxExponent);
-        innerSlider.setSkewFactorFromMidPoint(
-                jmap(0.90f, TailOff::minExponent, TailOff::maxExponent));
-        slider.onValueChange = [this, &innerSlider]() {
-            auto exponent = innerSlider.getValue();
-            loudnessAnalyser.decayLength.setExponent((float) exponent);
-        };
-    }
-
-
-    // ====================================================
-    // MultiTimer functions
-    // ====================================================
-
     float _lastLevelSent = -10.f; // set to strange value to start off with
 
     void updateCPULabel() {
         const auto cpuPercent = deviceManager.getCpuUsage() * 100.0f;
         cpuUsageText.setText(String(cpuPercent, 6) + " %", dontSendNotification);
     }
-
-    // ===============================
-    // OSC functions
-    // ===============================
 
     void setRemoteAddressFromString(const String &text) {
         if (sender.connect(text, 9000)) {
@@ -262,7 +167,6 @@ private:
     RemoteAddressComponent addressComponent;
 
     // ===============================
-
     LoudnessAnalyser loudnessAnalyser{[this](float level) {
         // always show level in history component
         valueHistoryComp.addLevel(level);
@@ -272,7 +176,11 @@ private:
             sender.sendLoudness(level);  // TODO: handle failed sends
             _lastLevelSent = level;
         }
-    }, initialProcessRateHz, initialProcessingBandLow, initialProcessingBandHigh, movingAverageInitialWindow, initialDecayExponent};
+    }, initialProcessRateHz, initialProcessingBandLow, initialProcessingBandHigh, movingAverageInitialWindow,
+                                      initialDecayExponent};
+    LoudnessAnalyserSettings loudnessAnalyserSettings{loudnessAnalyser, initialProcessRateHz, initialProcessingBandLow,
+                                                       initialProcessingBandHigh, movingAverageInitialWindow,
+                                                       initialDecayExponent};
 
     StdoutLogger logger{};
     AudioApp::AudioSourceComponent audioSource{deviceManager, logger};
@@ -280,19 +188,11 @@ private:
     Label cpuUsageLabel{"CPU Usage", "CPU Usage"};
     Label cpuUsageText;
 
-    LabelledSlider shaperInSlider{"Range In"};
+    ValueHistoryComponent valueHistoryComp;
 
     static constexpr float initialProcessRateHz = 50.f;
-    LabelledSlider processRateSlider{"Process rate"};
-
-    LabelledSlider decayLengthSlider{"Decay Length"};
     static constexpr float initialDecayExponent = 0.8f;
-
-    ValueHistoryComponent valueHistoryComp;
-    LabelledSlider movingAverageSlider{"Window Size"};
     static constexpr int movingAverageInitialWindow = 2;
-
-    LabelledSlider processingBandSlider{"Frequency Band"};
     static constexpr double initialProcessingBandLow = 0.02;
     static constexpr double initialProcessingBandHigh = 0.13;
 
