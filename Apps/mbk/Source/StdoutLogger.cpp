451565fa83291ea17e281af2d7a6a9b3aece0ec6
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2022-11-27 18:09:27 +0000

    Support debug/non-debug mode in StdoutLogger

--- Apps/mbk/Source/StdoutLogger.cpp
+++ Apps/mbk/Source/StdoutLogger.cpp
@@ -4,15 +4,22 @@
 
 #include "StdoutLogger.h"
 
+StdoutLogger::StdoutLogger(){
+    _debug = true;
+}
+StdoutLogger::StdoutLogger(bool debug){
+    _debug = debug;
+}
+
 void StdoutLogger::debug(const juce::String &message) {
-    std::cout << "DEBUG: " << message.toStdString() << std::endl;
+    if (this->_debug) {
+        std::cout << "DEBUG: " << message.toStdString() << std::endl;
+    }
 }
 
 void StdoutLogger::info(const juce::String &message) {
     std::cout << "INFO : " << message.toStdString() << std::endl;
 }
-
 void StdoutLogger::error(const juce::String &message) {
     std::cout << "ERROR: " << message.toStdString() << std::endl;
 }
-
