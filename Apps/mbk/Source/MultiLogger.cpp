28a8209a5d5c70e6ce4eb20766a685576da54eac
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2022-12-10 22:32:49 +0000

    Log to stdout and UI

---
+++ Apps/mbk/Source/MultiLogger.cpp
@@ -0,0 +1,27 @@
+//
+// Created by glynh on 10/12/2022.
+//
+
+#include "MultiLogger.h"
+
+namespace AudioApp
+{
+MultiLogger::MultiLogger(const std::vector<Logger*>& loggers){
+    _loggers = loggers;
+}
+void MultiLogger::debug(const juce::String &message) {
+    for (auto& _logger: _loggers) {
+        _logger->debug(message);
+    }
+}
+void MultiLogger::info(const juce::String &message) {
+    for (auto& _logger: _loggers) {
+        _logger->info(message);
+    }
+}
+void MultiLogger::error(const juce::String &message) {
+    for (auto& _logger: _loggers) {
+        _logger->error(message);
+    }
+}
+} // namespace AudioApp
---
+++ Apps/mbk/Source/MultiLogger.h
@@ -0,0 +1,27 @@
+//
+// Created by glynh on 10/12/2022.
+//
+
+#ifndef JUCECMAKEREPO_MULTILOGGER_H
+#define JUCECMAKEREPO_MULTILOGGER_H
+
+#include "Logger.h"
+
+namespace AudioApp
+{
+
+class MultiLogger : public Logger
+{
+public:
+    explicit MultiLogger(const std::vector<Logger*>& loggers);
+    void debug(const juce::String& message) override;
+    void info(const juce::String& message) override;
+    void error(const juce::String& message) override;
+
+private:
+    std::vector<Logger*> _loggers;
+};
+
+} // namespace AudioApp
+
+#endif //JUCECMAKEREPO_MULTILOGGER_H
