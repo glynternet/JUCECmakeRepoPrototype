4da8bca60e2eb10a26134ca9f86cc7d9be15ca96
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2023-01-02 17:06:21 +0000

    Fix some lint warnings

--- Apps/mbk/Source/Loudness/ValueShaper.h
+++ Apps/mbk/Source/Loudness/ValueShaper.h
@@ -12,12 +12,12 @@ public:
         , _outMax(outMax)
     {
     }
-    ~ValueShaper() {}
+    ~ValueShaper() = default;
 
     void setInMin(float value) { _inMin = value; }
     void setInMax(float value) { _inMax = value; }
 
-    float shape(float value)
+    [[nodiscard]] float shape(float value) const
     {
         // TODO: allow configuration of this from the GUI in some advanced settings
         return jlimit(0.0f, 1.0f, jmap(value, _inMin, _inMax, _outMin, _outMax));
