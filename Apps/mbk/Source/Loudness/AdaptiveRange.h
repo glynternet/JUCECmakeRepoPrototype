6d5db69b8e46432b486600bc72bc932608e1cdf4
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2026-01-10 22:42:27 +0000

    Range min separation of 3

--- Apps/mbk/Source/Loudness/AdaptiveRange.h
+++ Apps/mbk/Source/Loudness/AdaptiveRange.h
@@ -33,7 +33,7 @@ public:
     AdaptiveRange(float initialMin,
                   float initialMax,
                   float adaptRate,
-                  float minSep = 0.2f)
+                  float minSep = 0.3F)
         : observedMin(initialMin)
         , observedMax(initialMax)
         , alpha(adaptRate)
