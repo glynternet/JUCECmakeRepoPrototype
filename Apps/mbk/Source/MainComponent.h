d2e5857a1cf3812d6bac0195dff36a5150dded0d
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2026-01-10 22:59:35 +0000

    v2.2.0

--- Apps/mbk/Source/MainComponent.h
+++ Apps/mbk/Source/MainComponent.h
@@ -29,7 +29,7 @@ private:
     LogOutputComponent uiLogger;
     logger::MultiLogger logger {{&stdoutLogger, &uiLogger}};
 
-    juce::Label versionLabel {"versionLabel", "version: v2.1.0"};
+    juce::Label versionLabel {"versionLabel", "version: v2.2.0"};
     OSCComponent oscComponent {logger};
     AvvaOSCSender oscSender {oscComponent};
     AudioSourceComponent audioSource {deviceManager, logger};
